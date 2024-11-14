655591df08c87710865112c978f02a5f9880c37d
hoatrinh <hoatv.7604@gmail.com>
2024-11-15 02:39:04 +0700

    * UPdate

--- index.js
+++ index.js
@@ -93,7 +93,7 @@
     streamingAssetsUrl: "StreamingAssets",
     companyName: "Tuk",
     productName: "Jobnalytics",
-    productVersion: "1.0.2",
+    productVersion: "1.0.3",
     showBanner: unityShowBanner,
 	cacheControl: function (url) {
   //return "immutable";
